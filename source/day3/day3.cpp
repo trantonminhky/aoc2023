32ddcf76c5f6b98c7b1c9870b6a6d11699cdc22d
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-09-30 16:58:56 +0700

    completed day 3 part 2

--- source/day3/day3.cpp
+++ source/day3/day3.cpp
@@ -166,3 +166,129 @@ void Day3::solve1(std::string input)
 	}
 	printf("result: %d\n", sum);
 }
+
+void Day3::solve2(std::string input)
+{
+	auto tokens = split(input, "\n");
+	std::vector<pairOfpartNumbersAndTheirCoordinatesType> partNumbersAndTheirCoordinates;
+
+	const int LENGTH_X = tokens[0].size();
+	const int LENGTH_Y = tokens.size();
+
+	// find all numbers and their coordinates
+	for (auto y = 0; y < LENGTH_Y; y++)
+	{
+		auto token = tokens[y];
+		std::stringstream buffer{};
+		pairOfCoordinatesXYType coordinates(0, 0);
+		buffer << '0';
+		for (auto x = 0; x < LENGTH_X; x++)
+		{
+			auto c = token[x];
+			if (isdigit(c) && std::stoi(buffer.str()) == 0)
+			{
+				coordinates = {x, y};
+				buffer << c;
+			}
+			else if (isdigit(c) && std::stoi(buffer.str()) != 0)
+			{
+				buffer << c;
+			}
+			else if (!isdigit(c) && std::stoi(buffer.str()) != 0)
+			{
+				auto partNumber = std::stoi(buffer.str());
+				pairOfCoordinatesAndWhetherTheyAppeared coorAndFlag = {coordinates, false};
+				partNumbersAndTheirCoordinates.push_back(pairOfpartNumbersAndTheirCoordinatesType(partNumber, coorAndFlag));
+				buffer.str("");
+				buffer.clear();
+				buffer << '0';
+			}
+		}
+		if (std::stoi(buffer.str()) != 0)
+		{
+			auto partNumber = std::stoi(buffer.str());
+			pairOfCoordinatesAndWhetherTheyAppeared coorAndFlag = {coordinates, false};
+			partNumbersAndTheirCoordinates.push_back(pairOfpartNumbersAndTheirCoordinatesType(partNumber, coorAndFlag));
+		}
+	}
+
+	// for (const auto& pair : partNumbersAndTheirCoordinates)
+	// {
+	// 	printf("<%d, %d>: %d\n", pair.second.first.first, pair.second.first.second, pair.first);
+	// }
+
+	auto sum = 0;
+
+	for (auto y = 0; y < LENGTH_Y; y++)
+	{
+		auto token = tokens[y];
+		for (auto x = 0; x < LENGTH_X; x++)
+		{
+			auto c = token[x];
+			if (c != '*') continue;
+			// printf("found symbol %c at ln %d col %d\n", c, y + 1, x + 1);
+
+			auto neighbors = Neighbor
+			{
+				pairOfCoordinatesXYType(x, std::max(y - 1, 0)),
+				pairOfCoordinatesXYType(std::min(x + 1, LENGTH_X), std::max(y - 1, 0)),
+				pairOfCoordinatesXYType(std::min(x + 1, LENGTH_X), y),
+				pairOfCoordinatesXYType(std::min(x + 1, LENGTH_X), std::min(y + 1, LENGTH_Y)),
+				pairOfCoordinatesXYType(x, std::min(y + 1, LENGTH_Y)),
+				pairOfCoordinatesXYType(std::max(x - 1, 0), std::min(y + 1, LENGTH_Y)),
+				pairOfCoordinatesXYType(std::max(x - 1, 0), y),
+				pairOfCoordinatesXYType(std::max(x - 1, 0), std::max(y - 1, 0))
+			};
+			std::unordered_map<int, pairOfCoordinatesAndWhetherTheyAppeared> adjacentParts;
+
+			std::vector<pairOfCoordinatesXYType> neighborsVector{ neighbors.n, neighbors.ne, neighbors.e, neighbors.se, neighbors.s, neighbors.sw, neighbors.w, neighbors.nw };
+
+			for (const auto& neighbor : neighborsVector)
+			{
+				char n_c = tokens[neighbor.second][neighbor.first];
+				if (!isdigit(n_c)) continue;
+
+				// printf("valid neighbor found at ln %d col %d: %c\n", neighbor.second + 1, neighbor.first + 1, n_c);
+
+				auto nearest = INT_MAX;
+				for (const auto& pair : partNumbersAndTheirCoordinates)
+				{
+					// printf("comparing current (<%d, %d>) and pair (<%d, %d>)\n", neighbor.first, neighbor.second, pair.second.first.first, pair.second.first.second);
+					if (neighbor.second != pair.second.first.second) continue;
+					if (neighbor.first < pair.second.first.first) continue;
+					if (nearest < neighbor.first - pair.second.first.first) continue;
+
+					nearest = neighbor.first - pair.second.first.first;
+					if (nearest == 0) break;
+				}
+
+				for (auto& pair : partNumbersAndTheirCoordinates)
+				{
+					if (pair.second.first == pairOfCoordinatesXYType(neighbor.first - nearest, neighbor.second))
+					{
+						adjacentParts.insert(pair);
+						break;
+					}
+				}
+				// printf("neighbor found at [%d, %d] has nearest value of %d\n", neighbor.first, neighbor.second, nearest);
+			}
+
+			if (adjacentParts.size() == 2)
+			{
+				std::vector<int> _adjacentParts;
+				for (const auto& part : adjacentParts)
+				{
+					_adjacentParts.push_back(part.first);
+				}
+				printf("part numbers %d and %d\n", _adjacentParts[0], _adjacentParts[1]);
+				sum += _adjacentParts[0] * _adjacentParts[1];
+			}
+		}
+	}
+
+	// for (const auto& pair : partNumbersAndTheirCoordinates)
+	// {
+	// 	printf("<%d, %d>: %d\n", pair.second.first.first, pair.second.first.second, pair.first);
+	// }
+	printf("result: %d\n", sum);
+}
